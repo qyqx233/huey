694b15597065ed0adcb225ae1b355e04a3f61f8f
zhou.han <zhou.han@yuancore.com>
2022-01-20 13:28:55 +0800

    添加rpc实现

--- huey/rpc.py
+++ huey/rpc.py
@@ -1,9 +1,17 @@
+import abc
+import functools
+import inspect
+import json
+from typing import Any, Type
+
 import aiohttp
+from loguru import logger
+from pydantic import BaseModel
 
 
-class BaseComm(object):
-    def __init__(self, name='huey'):
-        self.name = name
+class Comm(object):
+    def __init__(self):
+        pass
 
     def close(self):
         """
@@ -13,28 +21,109 @@ class BaseComm(object):
         """
         pass
 
-    def send(self):
+    def exchange_sync(self):
         pass
 
-    def recv(self):
+    async def exchange(self, endpoint: str, data: bytes) -> bytes:
         pass
 
-    async def send_async(self):
-        pass
-
-    async def recv_async(self):
+    async def recv(self):
         pass
 
 
-class HTTPComm(BaseComm):
-    def __init__(self, name, host, port):
-        super(HTTPComm, self).__init__(name)
+class HTTPComm(Comm):
+    def __init__(self, host, port):
+        super(HTTPComm, self).__init__()
         self.conn = aiohttp.TCPConnector(verify_ssl=False)
-        self.host_port = f"{host}:{port}"
+        self.host_port = f"http://{host}:{port}"  # noqa
 
-    async def comm_async(self, endpoint: str, data: bytes) -> bytes:
+    async def exchange(self, endpoint: str, data: bytes) -> bytes:
         async with aiohttp.ClientSession(connector=self.conn) as session:
-            async with session.get(f"{self.host_port}/{endpoint}") as r:
+            target = f"{self.host_port}{endpoint}"
+            async with session.post(target, data=data) as r:
                 if r._body is None:  # noqa
                     await r.read()
                 return r._body  # noqa
+
+
+class Serializer:
+    @abc.abstractmethod
+    def serialize(self, obj: Any) -> bytes:
+        pass
+
+    @abc.abstractmethod
+    def deserialize(self, data: bytes, t: Type = None) -> Any:
+        pass
+
+
+class PydanticSerializer(Serializer):
+    def serialize(self, obj: BaseModel) -> bytes:
+        return obj.json().encode("utf8")
+
+    def deserialize(self, data: bytes, t: BaseModel = None) -> BaseModel:
+        return t.parse_obj(json.loads(data.decode("utf8")))
+
+
+class JSONSerializer(Serializer):
+    def __init__(self):
+        Serializer.__init__(self)
+
+    def serialize(self, obj: Any) -> bytes:
+        return json.dumps(obj).encode("utf8")
+
+    def deserialize(self, data: bytes, t: Type = None) -> Any:
+        return json.loads(data.decode("utf8"))
+
+
+class CallWrapper:
+    def __call__(self, func):
+        pass
+
+
+class Rpc:
+    serializer: Serializer
+    comm: Comm
+
+    def __init__(self):
+        pass
+
+    # def serialize(self, obj):
+    #     return self.serializer.serialize(obj)
+
+    async def request(self, endpoint, obj, t: Any):
+        serializer = self.serializer
+        req_data = serializer.serialize(obj)
+        logger.debug("req_data={}", req_data)
+        rsp_data = await self.comm.exchange(endpoint, req_data)
+        logger.debug(rsp_data)
+        return serializer.deserialize(rsp_data, t)
+
+    def call_wrapper(self, endpoint, response_class: Any = None):
+        that = self
+
+        class Deco:
+            def __init__(self, fn):
+                self.sig = inspect.signature(fn)
+                print(self.sig)
+                self.fn = fn
+                self.rpc = that
+
+            async def __call__(self, *args):
+                req = {k: v for k, v in zip(self.sig.parameters, args)}
+                return await self.rpc.request(endpoint, req, response_class)
+
+        return Deco
+
+
+class JSONRpc(Rpc):
+    def __init__(self, comm):
+        Rpc.__init__(self)
+        self.serializer = JSONSerializer()
+        self.comm = comm
+
+
+class PydanticRpc(Rpc):
+    def __init__(self, comm):
+        Rpc.__init__(self)
+        self.serializer = PydanticSerializer()
+        self.comm = comm
---
+++ huey/tests1/test_http.py
@@ -0,0 +1,100 @@
+import asyncio
+import inspect
+from typing import List, Optional
+
+from loguru import logger
+from pydantic import BaseModel
+
+from huey.rpc import HTTPComm, PydanticRpc, JSONRpc
+
+
+class CmdRequest(BaseModel):
+    name: str
+    args: Optional[List[str]]
+
+
+class CmdResponse(BaseModel):
+    code: int
+    msg: str
+    data: Optional[str]
+
+
+def test_pydantic():
+    cmd = CmdRequest(name="aa", args=["ls"])
+    print(cmd.json().__class__)
+    print(cmd.json())
+
+
+def test():
+    async def fx():
+        client = PydanticRpc(HTTPComm("192.168.50.76", 3212))
+        r = await client.request("/", CmdRequest(name="ls"), CmdResponse)
+        logger.debug("xxx {}", r.__class__)
+
+    asyncio.get_event_loop().run_until_complete(fx())
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def w(a):
+    def deco(fn):
+        def wrapper(*args):
+            return fn(*args)
+
+        return wrapper
+
+    return deco
+
+
+def ww(a):
+    class Deco:
+        def __init__(self, fn):
+            self.fn = fn
+            print()
+            print("ssssss", asyncio.iscoroutinefunction(fn))
+
+        def __call__(self, *args, ):
+            return self.fn(*args)
+
+    return Deco
+
+
+# @ww(100)
+async def foo(a, b, *v, c=100):
+    return a + b
+
+
+def test_add():
+    async def fx():
+        sig = inspect.signature(foo)
+        print()
+        # print(await foo(1, 2))
+
+        for k in sig.parameters:
+            v = sig.parameters[k]
+            print("==", k, v.name, v.empty, v.default, v.annotation)
+
+    asyncio.get_event_loop().run_until_complete(fx())
+
+
+rpc = JSONRpc(HTTPComm("192.168.50.76", 3212))
+
+
+def no_arg():
+    pass
+
+
+@rpc.call_wrapper("/", )
+def ff(name: str, args: List[str]):
+    pass
+
+
+def test_send():
+    async def fx():
+        await ff("ls", ["/"])
+        # logger.debug(inspect.signature(no_arg).__class__)
+        pass
+
+    asyncio.get_event_loop().run_until_complete(fx())
